0f293f9c94edc01f61de0a6594cda6c8ecd86cd1
Fardous kabir <kabirfardous143@gmail.com>
2023-02-07 21:27:22 -0500

    first commit

---
+++ src/demo/Hellowrold.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Hellowrold {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("i love bangladesh");
+         System.out.println("this is to much");
+         System.out.println("I like to do IT");
+	}
+
+}
